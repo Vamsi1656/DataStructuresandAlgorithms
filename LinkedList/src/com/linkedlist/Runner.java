67971dbe40d02d522b8842cacedae27be0c48d55
Vamsi1656 <vamsireddy1717@gmail.com>
2024-09-04 17:31:55 +0530

    ok

---
+++ LinkedList/src/com/linkedlist/Runner.java
@@ -0,0 +1,17 @@
+package com.linkedlist;
+
+
+public class Runner {
+	
+	public static void main(String args[]) {
+		LinkedList list=new LinkedList();
+		list.insert(18);
+		list.insert(45);
+		list.insert(7);
+		
+		list.show();
+		
+		
+	}
+
+}
